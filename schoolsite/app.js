5bc055114ce26172871331159e87c7edc49fd250
EmielEDW <emieldewaele@gmail.com>
2023-04-17 21:41:14 +0200

    Files aangemaakt met code

---
+++ schoolsite/app.js
@@ -0,0 +1,16 @@
+const studierichtingenContainer = document.getElementById("studierichtingen");
+
+fetch("studierichtingen.json")
+	.then(response => response.json())
+	.then(studierichtingen => {
+		studierichtingen.forEach(studierichting => {
+			const studierichtingElement = document.createElement("div");
+			studierichtingElement.classList.add("studierichting");
+			studierichtingElement.innerHTML = `
+				<img src="${studierichting.afbeelding}">
+				<h2>${studierichting.naam}</h2>
+				<p>${studierichting.beschrijving}</p>
+			`;
+			studierichtingenContainer.appendChild(studierichtingElement);
+		});
+	});
